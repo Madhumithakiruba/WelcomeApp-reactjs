e9ea9f078701e74d5642d447eedd542d8c2b3fe3
Madhu_mitha <madhukiruba99@gmail.com>
2023-11-09 06:57:54 +0000

    Welcome App

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,33 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  onSubscribe = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+
+    return isSubscribed ? 'subscribed' : 'subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+    return (
+      <div className="app-container">
+        <div className="welcome-container">
+          <h1 className="heading">Welcome</h1>
+          <p className="text">Thankyou! Happy Learning</p>
+          <button type="button" className="button" onClick={this.onSubscribe}>
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default Welcome
